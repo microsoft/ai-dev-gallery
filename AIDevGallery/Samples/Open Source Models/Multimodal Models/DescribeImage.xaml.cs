87bba0128891ee8a03c3baa646567f9368544b82
Jaylyn Barbee <51131738+Jaylyn-Barbee@users.noreply.github.com>
2025-01-21 15:36:41 -0500

    Update AIDevGallery/Samples/Open Source Models/Multimodal Models/DescribeImage.xaml.cs
    Co-authored-by: Zach Teutsch <88554871+zateutsch@users.noreply.github.com>

--- AIDevGallery/Samples/Open Source Models/Multimodal Models/DescribeImage.xaml.cs
+++ AIDevGallery/Samples/Open Source Models/Multimodal Models/DescribeImage.xaml.cs
@@ -170,7 +170,6 @@ internal sealed partial class DescribeImage : BaseSamplePage
             imageFile = file;
             LoadImage(imageFile);
 
-            // Run inference
             await DescribeTheImage(imageFile);
         }
     }
