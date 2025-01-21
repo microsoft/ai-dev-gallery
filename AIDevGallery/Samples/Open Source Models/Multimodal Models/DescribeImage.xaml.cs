ca2b614b631d1c71542c058a274d94f9b166b102
Jaylyn Barbee <51131738+Jaylyn-Barbee@users.noreply.github.com>
2025-01-21 15:36:59 -0500

    Update AIDevGallery/Samples/Open Source Models/Multimodal Models/DescribeImage.xaml.cs
    Co-authored-by: Zach Teutsch <88554871+zateutsch@users.noreply.github.com>

--- AIDevGallery/Samples/Open Source Models/Multimodal Models/DescribeImage.xaml.cs
+++ AIDevGallery/Samples/Open Source Models/Multimodal Models/DescribeImage.xaml.cs
@@ -140,9 +140,9 @@ internal sealed partial class DescribeImage : BaseSamplePage
         {
             ct.ThrowIfCancellationRequested();
 
-            // This step takes a long time, theoretically, most cancellation get hung here
             await Task.Delay(0, ct).ConfigureAwait(false);
-
+            
+            // This step takes a long time, theoretically, most cancellation get hung here
             generator.ComputeLogits();
             ct.ThrowIfCancellationRequested();
 
