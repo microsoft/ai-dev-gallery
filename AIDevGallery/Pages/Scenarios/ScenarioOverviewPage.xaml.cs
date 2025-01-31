4abe331390a17a52a8ce43ed3d2f58a911bab05d
Niels Laute <niels.laute@live.nl>
2025-01-31 16:40:56 +0100

    Changes

--- AIDevGallery/Pages/Scenarios/ScenarioOverviewPage.xaml.cs
+++ AIDevGallery/Pages/Scenarios/ScenarioOverviewPage.xaml.cs
@@ -3,25 +3,61 @@
 
 using AIDevGallery.Models;
 using AIDevGallery.Samples;
+using AIDevGallery.Utils;
+using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Navigation;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
 namespace AIDevGallery.Pages;
 
 internal sealed partial class ScenarioOverviewPage : Page
 {
+    private ObservableCollection<ScenarioCategory> allScenarioCategories;
+    private ObservableCollection<MostRecentlyUsedItem> mostRecentlyUsedCategories;
+
     public ScenarioOverviewPage()
     {
         this.InitializeComponent();
+        allScenarioCategories = new ObservableCollection<ScenarioCategory>();
+        mostRecentlyUsedCategories = new ObservableCollection<MostRecentlyUsedItem>();
     }
 
     protected override void OnNavigatedTo(NavigationEventArgs e)
     {
         base.OnNavigatedTo(e);
 
-        allView.ItemsSource = ScenarioCategoryHelpers.AllScenarioCategories;
+        foreach (var category in ScenarioCategoryHelpers.AllScenarioCategories)
+        {
+            allScenarioCategories.Add(category);
+        }
+
+        //List<Scenario> recentScenarios = new List<Scenario>();
+
+        //foreach (var mru in App.AppData.MostRecentlyUsedItems)
+        //{
+        //    if (mru.Type == Utils.MostRecentlyUsedItemType.Scenario)
+        //    {
+        //        recentScenarios.Add(App.FindScenarioById(mru.ItemId));
+        //    }
+        //}
+
+        //ScenarioCategory recentCategory = new ScenarioCategory
+        //{
+        //    Name = "Recent",
+        //    Description = "Recently used scenarios",
+        //    Icon = "\uE81C",
+        //    Scenarios = recentScenarios
+        //};
+
+        //if (recentScenarios.Count > 0)
+        //{
+        //    allScenarioCategories.Insert(0, recentCategory);
+        //}
     }
 
-    private void ItemsView_ItemInvoked(ItemsView sender, ItemsViewItemInvokedEventArgs args)
+    private void ScenarioItemsView_ItemInvoked(ItemsView sender, ItemsViewItemInvokedEventArgs args)
     {
         if (args.InvokedItem is Scenario scenario)
         {
