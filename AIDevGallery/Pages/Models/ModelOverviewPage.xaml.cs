0aa04d049e3192d6abee093bc1719e553fd384c3
Niels Laute <niels.laute@live.nl>
2025-02-03 15:51:54 +0100

    Models page

---
+++ AIDevGallery/Pages/Models/ModelOverviewPage.xaml.cs
@@ -0,0 +1,10 @@
+using Microsoft.UI.Xaml.Controls;
+
+namespace AIDevGallery.Pages;
+public sealed partial class ModelOverviewPage : Page
+{
+    public ModelOverviewPage()
+    {
+        this.InitializeComponent();
+    }
+}
