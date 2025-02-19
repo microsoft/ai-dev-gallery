e4582c711ad1c04b1b835e3e0bf3e6c2b5fae3eb
Nikola Metulev <711864+nmetulev@users.noreply.github.com>
2025-02-18 17:26:24 -0800

    Added missing events

---
+++ AIDevGallery/Telemetry/Events/WcrApiDownloadFailedEvent.cs
@@ -0,0 +1,46 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using AIDevGallery.Models;
+using Microsoft.Diagnostics.Telemetry;
+using Microsoft.Diagnostics.Telemetry.Internal;
+using System;
+using System.Diagnostics.Tracing;
+
+namespace AIDevGallery.Telemetry.Events;
+
+[EventData]
+internal class WcrApiDownloadFailedEvent : EventBase
+{
+    internal WcrApiDownloadFailedEvent(ModelType apiType, string errorMessage, DateTime errorTime)
+    {
+        ApiType = apiType;
+        ErrorMessage = errorMessage;
+        ErrorTime = errorTime;
+    }
+
+    public ModelType ApiType { get; private set; }
+
+    public DateTime ErrorTime { get; private set; }
+
+    public string ErrorMessage { get; set; }
+
+    public override PartA_PrivTags PartA_PrivTags => PrivTags.ProductAndServiceUsage;
+
+    public override void ReplaceSensitiveStrings(Func<string?, string?> replaceSensitiveStrings)
+    {
+    }
+
+    public static void Log(ModelType apiType, string errorMessage)
+    {
+        var relatedActivityId = Guid.NewGuid();
+        TelemetryFactory.Get<ITelemetry>().LogError("WcrApiDownloadFailed_Event", LogLevel.Critical, new WcrApiDownloadFailedEvent(apiType, errorMessage, DateTime.Now), relatedActivityId);
+    }
+
+    public static void Log(ModelType apiType, Exception ex)
+    {
+        var relatedActivityId = Guid.NewGuid();
+        TelemetryFactory.Get<ITelemetry>().LogError("WcrApiDownloadFailed_Event", LogLevel.Critical, new WcrApiDownloadFailedEvent(apiType, ex.Message, DateTime.Now), relatedActivityId);
+        TelemetryFactory.Get<ITelemetry>().LogException("WcrApiDownloadFailed_Event", ex, relatedActivityId);
+    }
+}
---
+++ AIDevGallery/Telemetry/Events/WcrApiDownloadRequestedEvent.cs
@@ -0,0 +1,38 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using AIDevGallery.Models;
+using Microsoft.Diagnostics.Telemetry;
+using Microsoft.Diagnostics.Telemetry.Internal;
+using System;
+using System.Diagnostics.Tracing;
+
+namespace AIDevGallery.Telemetry.Events;
+
+[EventData]
+internal class WcrApiDownloadRequestedEvent : EventBase
+{
+    internal WcrApiDownloadRequestedEvent(ModelType apiType, string? sampleId, DateTime startTime)
+    {
+        ApiType = apiType;
+        SampleId = sampleId;
+        StartTime = startTime;
+    }
+
+    public ModelType ApiType { get; private set; }
+
+    public DateTime StartTime { get; private set; }
+
+    public string? SampleId { get; set; }
+
+    public override PartA_PrivTags PartA_PrivTags => PrivTags.ProductAndServiceUsage;
+
+    public override void ReplaceSensitiveStrings(Func<string?, string?> replaceSensitiveStrings)
+    {
+    }
+
+    public static void Log(ModelType apiType, string? sampleId = null)
+    {
+        TelemetryFactory.Get<ITelemetry>().Log("WcrApiDownloadRequested_Event", LogLevel.Critical, new WcrApiDownloadRequestedEvent(apiType, sampleId, DateTime.Now));
+    }
+}
