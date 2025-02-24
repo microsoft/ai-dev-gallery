182beee4541a32f760c2f02c0e863c63cdc4ed00
Niels Laute <niels.laute@live.nl>
2025-02-24 15:29:07 +0100

    New homepage

--- AIDevGallery/Pages/HomePage.xaml.cs
+++ AIDevGallery/Pages/HomePage.xaml.cs
@@ -1,15 +1,21 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+using AIDevGallery.Models;
 using AIDevGallery.Telemetry;
 using AIDevGallery.Telemetry.Events;
+using AIDevGallery.Utils;
+using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Navigation;
+using System.Collections.ObjectModel;
 
 namespace AIDevGallery.Pages;
 
 internal sealed partial class HomePage : Page
 {
+    private ObservableCollection<MostRecentlyUsedItem> mostRecentlyUsedItems = new ObservableCollection<MostRecentlyUsedItem>();
+
     public HomePage()
     {
         this.InitializeComponent();
@@ -21,20 +27,36 @@ internal sealed partial class HomePage : Page
         NavigatedToPageEvent.Log(nameof(HomePage));
     }
 
-    private void Page_Loaded(object sender, Microsoft.UI.Xaml.RoutedEventArgs e)
+    private void Page_Loaded(object sender, RoutedEventArgs e)
     {
         if (!App.AppData.IsDiagnosticsMessageDismissed && PrivacyConsentHelpers.IsPrivacySensitiveRegion())
         {
             DiagnosticsInfoBar.IsOpen = true;
         }
+
+        LoadRecentlyUsedItems();
     }
 
-    private void DiagnosticsYesButton_Click(object sender, Microsoft.UI.Xaml.RoutedEventArgs e)
+    private void LoadRecentlyUsedItems()
+    {
+        if (App.AppData.MostRecentlyUsedItems.Count > 0)
+        {
+            RecentPanel.Visibility = Visibility.Visible;
+
+            foreach (var item in App.AppData.MostRecentlyUsedItems)
+            {
+                item.Description = GetFirstSentenceFromDescription(item.Description);
+                mostRecentlyUsedItems.Add(item);
+            }
+        }
+    }
+
+    private void DiagnosticsYesButton_Click(object sender, RoutedEventArgs e)
     {
         HandleDiagnosticsSetting(true);
     }
 
-    private void DiagnosticsNoButton_Click(object sender, Microsoft.UI.Xaml.RoutedEventArgs e)
+    private void DiagnosticsNoButton_Click(object sender, RoutedEventArgs e)
     {
         HandleDiagnosticsSetting(false);
     }
@@ -45,5 +67,32 @@ internal sealed partial class HomePage : Page
         App.AppData.IsDiagnosticsMessageDismissed = true;
         App.AppData.IsDiagnosticDataEnabled = isEnabled;
         await App.AppData.SaveAsync();
+    }
+
+    private string GetFirstSentenceFromDescription(string? description)
+    {
+        if (description == null)
+        {
+            return string.Empty;
+        }
+
+        int i;
+        for (i = 0; i < description.Length; i++)
+        {
+            if (description[i] == '.')
+            {
+                break;
+            }
+        }
+
+        return i == description.Length ? description : description.Substring(0, i + 1);
+    }
+
+    private void MRUView_ItemInvoked(ItemsView sender, ItemsViewItemInvokedEventArgs args)
+    {
+        if (args.InvokedItem is MostRecentlyUsedItem mru)
+        {
+            App.MainWindow.NavigateToPage(mru);
+        }
     }
 }
