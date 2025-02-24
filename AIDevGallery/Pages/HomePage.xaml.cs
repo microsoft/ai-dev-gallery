294fd639c7d7995029b98b7fea43714c95b9d52d
Niels Laute <niels.laute@live.nl>
2025-02-24 17:49:14 +0100

    Adding responsiveness

--- AIDevGallery/Pages/HomePage.xaml.cs
+++ AIDevGallery/Pages/HomePage.xaml.cs
@@ -1,7 +1,6 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-using AIDevGallery.Models;
 using AIDevGallery.Telemetry;
 using AIDevGallery.Telemetry.Events;
 using AIDevGallery.Utils;
