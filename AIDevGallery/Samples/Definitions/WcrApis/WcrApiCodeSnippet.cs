48f2de55b09a4bf211c05a44de6f6f00c9520e64
Nikola Metulev <711864+nmetulev@users.noreply.github.com>
2025-05-08 18:21:16 -0700

    fixed code snippets and added copy button

--- AIDevGallery/Samples/Definitions/WcrApis/WcrApiCodeSnippet.cs
+++ AIDevGallery/Samples/Definitions/WcrApis/WcrApiCodeSnippet.cs
@@ -12,8 +12,9 @@ internal static class WcrApiCodeSnippet
     {
         {
             ModelType.PhiSilica, """"
+            using Microsoft.Windows.AI;
             using Microsoft.Windows.AI.Text;
-            
+
             var readyState = LanguageModel.GetReadyState();
             if (readyState is AIFeatureReadyState.Ready or AIFeatureReadyState.NotReady)
             {
@@ -21,19 +22,25 @@ internal static class WcrApiCodeSnippet
                 {
                     var op = await LanguageModel.EnsureReadyAsync();
                 }
-            
-                using LanguageModel languageModel = LanguageModel.CreateAsync();
-            
-                string prompt = "Provide the molecular formula for glucose.";
-            
-                var result = await languageModel.GenerateResponseAsync(prompt);
-            
-                Console.WriteLine(result.Response);
+
+                using LanguageModel languageModel = await LanguageModel.CreateAsync();
+
+                string prompt = "Tell me a short story";
+
+                var result = languageModel.GenerateResponseAsync(prompt);
+
+                result.Progress += (sender, args) =>
+                {
+                    Console.Write(args);
+                };
+
+                await result;
             }
             """"
         },
         {
             ModelType.PhiSilicaLora, """"
+            using Microsoft.Windows.AI;
             using Microsoft.Windows.AI.Text;
             using Microsoft.Windows.AI.Text.Experimental;
 
@@ -44,10 +51,10 @@ internal static class WcrApiCodeSnippet
                 {
                     var op = await LanguageModel.EnsureReadyAsync();
                 }
-            
-                using LanguageModel languageModel = LanguageModel.CreateAsync();
+
+                using LanguageModel languageModel = await LanguageModel.CreateAsync();
                 using LanguageModelExperimental loraModel = new LanguageModelExperimental(languageModel);
-              
+
                 string adapterFilePath = "path_to_your_adapter_file";
                 LowRankAdaptation loraAdapter = loraModel.LoadAdapter(adapterFilePath);
 
@@ -55,19 +62,20 @@ internal static class WcrApiCodeSnippet
                 {
                     LoraAdapter = loraAdapter
                 };
-            
+
                 string prompt = "Provide the molecular formula for glucose.";
-            
+
                 var result = await loraModel.GenerateResponseAsync(prompt, options);
-            
-                Console.WriteLine(result.Response);
+
+                Console.WriteLine(result.Text);
             }
             """"
         },
         {
             ModelType.TextRecognitionOCR, """"
-            using Microsoft.Windows.Vision;
             using Microsoft.Graphics.Imaging;
+            using Microsoft.Windows.AI.Imaging;
+            using Microsoft.Windows.AI;
 
             var readyState = TextRecognizer.GetReadyState();
             if (readyState is AIFeatureReadyState.Ready or AIFeatureReadyState.NotReady)
@@ -76,11 +84,11 @@ internal static class WcrApiCodeSnippet
                 {
                     var op = await TextRecognizer.EnsureReadyAsync();
                 }
-                        
-                using TextRecognizer textRecognizer = TextRecognizer.CreateAsync();
-            
-                ImageBuffer imageBuffer = ImageBuffer.CreateBufferAttachedToBitmap(bitmap);
-                RecognizedText? result = textRecognizer?.RecognizeTextFromImage(imageBuffer, new TextRecognizerOptions());
+
+                using TextRecognizer textRecognizer = await TextRecognizer.CreateAsync();
+
+                ImageBuffer imageBuffer = ImageBuffer.CreateForSoftwareBitmap(bitmap);
+                RecognizedText? result = textRecognizer?.RecognizeTextFromImage(imageBuffer);
 
                 Console.WriteLine(string.Join("\n", result.Lines.Select(l => l.Text)));
             }
@@ -88,7 +96,8 @@ internal static class WcrApiCodeSnippet
         },
         {
             ModelType.ImageScaler, """"
-            using Microsoft.Graphics.Imaging;
+            using Microsoft.Windows.AI.Imaging;
+            using Microsoft.Windows.AI;
             using Windows.Graphics.Imaging;
 
             var readyState = ImageScaler.GetReadyState();
@@ -98,7 +107,7 @@ internal static class WcrApiCodeSnippet
                 {
                     var op = await ImageScaler.EnsureReadyAsync();
                 }
-                        
+
                 ImageScaler imageScaler = await ImageScaler.CreateAsync();
                 SoftwareBitmap finalImage = imageScaler.ScaleSoftwareBitmap(softwareBitmap, targetWidth, targetHeight);
             }
@@ -106,7 +115,9 @@ internal static class WcrApiCodeSnippet
         },
         {
             ModelType.BackgroundRemover, """"
-            using Microsoft.Graphics.Imaging;
+            using Microsoft.Windows.AI;
+            using Microsoft.Windows.AI.Imaging;
+            using Windows.Graphics;
             using Windows.Graphics.Imaging;
 
             var readyState = ImageObjectExtractor.GetReadyState();
@@ -116,24 +127,27 @@ internal static class WcrApiCodeSnippet
                 {
                     var op = await ImageObjectExtractor.EnsureReadyAsync();
                 }
-            
+
                 ImageObjectExtractor imageObjectExtractor = await ImageObjectExtractor.CreateWithSoftwareBitmapAsync(softwareBitmap);
 
-                ImageObjectExtractorHint hint = new ImageObjectExtractorHint{
-                    includeRects: null, 
-                    includePoints:
-                        new List<PointInt32> { new PointInt32(306, 212),
-                                               new PointInt32(216, 336)},
-                    excludePoints: null};
-            
+                ImageObjectExtractorHint hint = new
+                (
+                    includeRects: null,
+                    includePoints: new List<PointInt32> { new PointInt32(306, 212), new PointInt32(216, 336) },
+                    excludePoints: null
+                );
+
                 SoftwareBitmap finalImage = imageObjectExtractor.GetSoftwareBitmapObjectMask(hint);
             }
             """"
         },
         {
             ModelType.ObjectRemover, """"
-            using Microsoft.Graphics.Imaging;
+            using Microsoft.Windows.AI.Imaging;
+            using Microsoft.Windows.AI;
             using Windows.Graphics.Imaging;
+            using System.Runtime.InteropServices.WindowsRuntime;
+            using Windows.Graphics;
 
             var readyState = ImageObjectRemover.GetReadyState();
             if (readyState is AIFeatureReadyState.Ready or AIFeatureReadyState.NotReady)
@@ -143,25 +157,44 @@ internal static class WcrApiCodeSnippet
                     var op = await ImageObjectRemover.EnsureReadyAsync();
                 }
 
-                var readyState = ImageObjectRemover.GetReadyState();
-            
+                readyState = ImageObjectRemover.GetReadyState();
+
                 ImageObjectRemover imageObjectRemover = await ImageObjectRemover.CreateAsync();
 
                 // Create a bitmap mask from the foreground rectangle - Gray Image with white rectangle
                 // The white rectangle is the area to be removed
-                var maskBitmap = CreateMaskFromForegroundRect(softwareBitmap.PixelWidth,softwareBitmap.PixelHeight, rect);
-                        
+                var maskBitmap = CreateMaskFromRect(softwareBitmap.PixelWidth, softwareBitmap.PixelHeight, rect);
+
                 SoftwareBitmap finalImage = imageObjectRemover.RemoveFromSoftwareBitmap(softwareBitmap, maskBitmap);
             }
+
+            // example of creating a mask from a rectangle
+            SoftwareBitmap CreateMaskFromRect(int width, int height, RectInt32 rect)
+            {
+                byte[] bitmapBuffer = new byte[width * height]; // Gray image hence 1-Byte per pixel.
+
+                for (var row = rect.Y; row < rect.Y + rect.Height; row++)
+                {
+                    for (var col = rect.X; col < rect.X + rect.Width; col++)
+                    {
+                        bitmapBuffer[row * width + col] = 255;
+                    }
+                }
+
+                SoftwareBitmap bitmap = new SoftwareBitmap(BitmapPixelFormat.Gray8, width, height, BitmapAlphaMode.Ignore);
+                bitmap.CopyFromBuffer(bitmapBuffer.AsBuffer());
+                return bitmap;
+            }
+            
             """"
         },
         {
             ModelType.ImageDescription, """"
             using Microsoft.Graphics.Imaging;
-            using Microsoft.Windows.AI.Text;
             using Microsoft.Windows.AI.ContentSafety;
-            using Windows.Graphics.Imaging;
-            
+            using Microsoft.Windows.AI.Imaging;
+            using Microsoft.Windows.AI;
+
             var readyState = ImageDescriptionGenerator.GetReadyState();
             if (readyState is AIFeatureReadyState.Ready or AIFeatureReadyState.NotReady)
             {
@@ -169,18 +202,18 @@ internal static class WcrApiCodeSnippet
                 {
                     var op = await ImageDescriptionGenerator.EnsureReadyAsync();
                 }
-            
+
                 ImageDescriptionGenerator imageDescriptionGenerator = await ImageDescriptionGenerator.CreateAsync();
-            
-                ImageBuffer inputImage = ImageBuffer.CreateCopyFromBitmap(softwareBitmap);  
-            
+
+                ImageBuffer inputImage = ImageBuffer.CreateForSoftwareBitmap(softwareBitmap);
+
                 ContentFilterOptions filterOptions = new ContentFilterOptions();
-                filterOptions.PromptMinSeverityLevelToBlock.ViolentContentSeverity = SeverityLevel.Medium;
-                filterOptions.ResponseMinSeverityLevelToBlock.ViolentContentSeverity = SeverityLevel.Medium;
-            
-                LanguageModelResponse languageModelResponse = await imageDescriptionGenerator.DescribeAsync(inputImage, ImageDescriptionScenario.Caption, filterOptions);
-            
-                Console.WriteLine(languageModelResponse.Response);
+                filterOptions.PromptMaxAllowedSeverityLevel.Violent = SeverityLevel.Medium;
+                filterOptions.ResponseMaxAllowedSeverityLevel.Violent = SeverityLevel.Medium;
+
+                ImageDescriptionResult languageModelResponse = await imageDescriptionGenerator.DescribeAsync(inputImage, ImageDescriptionKind.DiagramDescription, filterOptions);
+
+                Console.WriteLine(languageModelResponse.Description);
             }
             """"
         }
