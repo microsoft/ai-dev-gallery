56c9fc30600edec92fb30f49805783fb3908d1c2
Nikola Metulev <711864+nmetulev@users.noreply.github.com>
2025-05-15 22:22:19 -0700

    Fixed warnings

--- AIDevGallery/Controls/ModelPicker/ModelPickerViews/LemonadePickerView.xaml.cs
+++ AIDevGallery/Controls/ModelPicker/ModelPickerViews/LemonadePickerView.xaml.cs
@@ -8,7 +8,6 @@ using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Windows.ApplicationModel.DataTransfer;
