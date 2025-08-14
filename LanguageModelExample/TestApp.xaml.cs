6d009489202c7e521db59f544f0b1b050c214296
MillyWei <yuanwei@microsoft.com>
2025-08-14 13:48:32 +0800

    update

---
+++ LanguageModelExample/TestApp.xaml.cs
@@ -0,0 +1,17 @@
+using Microsoft.UI.Xaml;
+
+namespace LanguageModelExample
+{
+    public partial class TestApp : Application
+    {
+        public TestApp()
+        {
+            this.InitializeComponent();
+        }
+
+        protected override void OnLaunched(Microsoft.UI.Xaml.LaunchActivatedEventArgs args)
+        {
+            // 简单的测试，确保基本类型可以识别
+        }
+    }
+} 
