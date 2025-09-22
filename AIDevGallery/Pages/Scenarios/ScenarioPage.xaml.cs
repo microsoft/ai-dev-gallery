b3147c4b97d9c3d3dc12295dc461bd51f4cdd624
Zhaopeng Wang (from Dev Box) <zhaopengwang@microsoft.com>
2025-09-23 06:56:57 +0800

    delete LoRA and Disable LanguageModel WinMlButton

--- AIDevGallery/Pages/Scenarios/ScenarioPage.xaml.cs
+++ AIDevGallery/Pages/Scenarios/ScenarioPage.xaml.cs
@@ -211,6 +211,14 @@ internal sealed partial class ScenarioPage : Page
             UpdateWinMLFlyout();
 
             WinMlModelOptionsButton.Visibility = Visibility.Visible;
+
+            foreach (var model in modelDetails)
+            {
+                if (model != null && model.IsLanguageModel())
+                {
+                    WinMlModelOptionsButton.Visibility = Visibility.Collapsed;
+                }
+            }
         }
         else
         {
