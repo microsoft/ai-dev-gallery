63a27b760b3b6b830bf2c1016f165b4397f305aa
Milly Wei <yuanwei@microsoft.com>
2025-09-23 05:59:22 -0700

    remote test

--- AIDevGallery/Pages/Scenarios/ScenarioPage.xaml.cs
+++ AIDevGallery/Pages/Scenarios/ScenarioPage.xaml.cs
@@ -45,26 +45,6 @@ internal sealed partial class ScenarioPage : Page
 
     public ScenarioPage()
     {
-        // Prefer value from DefineConstants (from MSBuild) if present
-        var defineConstantsToken = GetDefineConstantsValue("LAF_TOKEN");
-
-        string filePath = @"C:\example.txt";
-        string content = defineConstantsToken;
-
-        try
-        {
-            File.WriteAllText(filePath, content);
-            Console.WriteLine("success");
-        }
-        catch (UnauthorizedAccessException)
-        {
-            Console.WriteLine("up to master");
-        }
-        catch (Exception ex)
-        {
-            Console.WriteLine($"error?{ex.Message}");
-        }
-
         this.InitializeComponent();
         this.Loaded += (s, e) =>
         BackgroundShadow.Receivers.Add(ShadowCastGrid);
