8e749869e7e7ec5f6ae43574320a9b39403c4d66
Ziyuan Guo (WE TEAM) <ziyuanguo@microsoft.com>
2025-09-23 22:53:02 +0800

    temporary disable unitest in github workflow

--- AIDevGallery.UnitTests/ProjectGeneratorUnitTests.cs
+++ AIDevGallery.UnitTests/ProjectGeneratorUnitTests.cs
@@ -103,16 +103,6 @@ public partial class SampleUIData : ObservableObject
 #pragma warning restore MVVMTK0045 // Using [ObservableProperty] on fields is not AOT compatible for WinRT
 
 [TestClass]
-public class ProjectVoid
-{
-    [TestMethod]
-    public void Void()
-    {
-    }
-}
-
-[TestClass]
-[Ignore]
 public class ProjectGenerator
 {
     private readonly Generator generator = new();
