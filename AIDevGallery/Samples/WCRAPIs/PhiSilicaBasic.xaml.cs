d9809b8282ff956ca2b04b5dc2e6da88efd3bbdb
Milly Way <181923419@qq.com>
2025-10-14 23:51:59 -0700

    Exported samples: replace LAF Token/PublisherId with demo values (#467)
    * Fix LAF bug in export sample
    
    * update mock value
    
    * lint
    
    ---------
    
    Co-authored-by: Milly Wei (from Dev Box) <yuanwei@microsoft.com>

--- AIDevGallery/Samples/WCRAPIs/PhiSilicaBasic.xaml.cs
+++ AIDevGallery/Samples/WCRAPIs/PhiSilicaBasic.xaml.cs
@@ -50,10 +50,7 @@ internal sealed partial class PhiSilicaBasic : BaseSamplePage
         // IMPORTANT!!
         // This is a demo LAF Token and PublisherId cannot be used for production code and won't be accepted in the Store
         // Please go to https://aka.ms/laffeatures to learn more and request a token for your app
-        // var demoToken = "Zv6LUQWEwhJTahzvwSGjHQ=="
         var demoToken = LimitedAccessFeaturesHelper.GetAiLanguageModelToken();
-
-        // var demoPublisherId = "z0sq19pdabnaj";
         var demoPublisherId = LimitedAccessFeaturesHelper.GetAiLanguageModelPublisherId();
 
         /*
