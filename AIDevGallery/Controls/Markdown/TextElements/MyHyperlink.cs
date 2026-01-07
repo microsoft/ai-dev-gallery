861ec7b33f92ac8cc1cf6c77df213c3202fed4a8
Milly Wei (from Dev Box) <yuanwei@microsoft.com>
2026-01-07 15:45:54 +0800

    fix

--- AIDevGallery/Controls/Markdown/TextElements/MyHyperlink.cs
+++ AIDevGallery/Controls/Markdown/TextElements/MyHyperlink.cs
@@ -40,7 +40,6 @@ internal class MyHyperlink : IAddChild
         var url = linkInline.GetDynamicUrl != null ? linkInline.GetDynamicUrl() ?? linkInline.Url : linkInline.Url;
         _linkInline = linkInline;
         _hyperlink = new Hyperlink();
-        SetNavigateUri(url);
     }
 
     public MyHyperlink(HtmlNode htmlNode, string? baseUrl)
@@ -49,33 +48,6 @@ internal class MyHyperlink : IAddChild
         var url = htmlNode.GetAttribute("href", "#");
         _htmlNode = htmlNode;
         _hyperlink = new Hyperlink();
-        SetNavigateUri(url);
-    }
-
-    private void SetNavigateUri(string? url)
-    {
-        if (string.IsNullOrEmpty(url))
-        {
-            return;
-        }
-
-        if (Uri.TryCreate(url, UriKind.RelativeOrAbsolute, out Uri? uri))
-        {
-            if (uri.IsAbsoluteUri)
-            {
-                _hyperlink.NavigateUri = uri;
-            }
-            else if (!string.IsNullOrEmpty(_baseUrl))
-            {
-                if (Uri.TryCreate(_baseUrl, UriKind.Absolute, out Uri? baseUri))
-                {
-                    if (Uri.TryCreate(baseUri, uri, out Uri? absoluteUri))
-                    {
-                        _hyperlink.NavigateUri = absoluteUri;
-                    }
-                }
-            }
-        }
     }
 
     public void AddChild(IAddChild child)
