a04fce0f7145c9a87d5c2a787c7f8719641c7352
Milly Wei (from Dev Box) <yuanwei@microsoft.com>
2026-01-07 12:32:42 +0800

    Add UT

--- AIDevGallery/Controls/Markdown/TextElements/MyHyperlink.cs
+++ AIDevGallery/Controls/Markdown/TextElements/MyHyperlink.cs
@@ -1,9 +1,9 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
-
 using HtmlAgilityPack;
 using Markdig.Syntax.Inlines;
 using Microsoft.UI.Xaml.Documents;
+using System;
 using Windows.Foundation;
 
 namespace CommunityToolkit.Labs.WinUI.MarkdownTextBlock.TextElements;
@@ -40,6 +40,7 @@ internal class MyHyperlink : IAddChild
         var url = linkInline.GetDynamicUrl != null ? linkInline.GetDynamicUrl() ?? linkInline.Url : linkInline.Url;
         _linkInline = linkInline;
         _hyperlink = new Hyperlink();
+        SetNavigateUri(url);
     }
 
     public MyHyperlink(HtmlNode htmlNode, string? baseUrl)
@@ -48,19 +49,47 @@ internal class MyHyperlink : IAddChild
         var url = htmlNode.GetAttribute("href", "#");
         _htmlNode = htmlNode;
         _hyperlink = new Hyperlink();
+        SetNavigateUri(url);
+    }
+
+    private void SetNavigateUri(string? url)
+    {
+        if (string.IsNullOrEmpty(url))
+        {
+            return;
+        }
+
+        if (Uri.TryCreate(url, UriKind.RelativeOrAbsolute, out Uri? uri))
+        {
+            if (uri.IsAbsoluteUri)
+            {
+                _hyperlink.NavigateUri = uri;
+            }
+            else if (!string.IsNullOrEmpty(_baseUrl) && Uri.TryCreate(_baseUrl, UriKind.Absolute, out Uri? baseUri))
+            {
+                if (Uri.TryCreate(baseUri, uri, out Uri? absoluteUri))
+                {
+                    _hyperlink.NavigateUri = absoluteUri;
+                }
+            }
+        }
     }
 
     public void AddChild(IAddChild child)
     {
-        if (child.TextElement is Microsoft.UI.Xaml.Documents.Inline inlineChild)
+        // Hyperlink cannot contain InlineUIContainer - this is a WinUI limitation
+        if (child.TextElement is not Microsoft.UI.Xaml.Documents.Inline inlineChild || inlineChild is InlineUIContainer)
         {
-            try
-            {
-                _hyperlink.Inlines.Add(inlineChild);
-            }
-            catch
-            {
-            }
+            return;
+        }
+
+        try
+        {
+            _hyperlink.Inlines.Add(inlineChild);
+        }
+        catch (ArgumentException ex)
+        {
+            System.Diagnostics.Debug.WriteLine($"[MyHyperlink] Failed to add {inlineChild.GetType().Name}: {ex.Message}");
         }
     }
 }
