ac82af42a60a1416e38d913cd8f2d5b04778d02d
Karen Lai <7976322+karkarl@users.noreply.github.com>
2026-01-14 09:33:38 -0800

    update apis

--- AIDevGallery/Samples/WCRAPIs/AppIndexCapability.xaml.cs
+++ AIDevGallery/Samples/WCRAPIs/AppIndexCapability.xaml.cs
@@ -4,7 +4,7 @@
 using AIDevGallery.Models;
 using AIDevGallery.Samples.Attributes;
 using Microsoft.UI.Xaml.Navigation;
-using Microsoft.Windows.AI.Search.Experimental.AppContentIndex;
+using Microsoft.Windows.Search.AppContentIndex;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
